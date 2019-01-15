8054dab5557ae59c1ce87e3607d938462fb990a6
FOStack <felix.o.stack@gmail.com>
2019-01-14 19:10:19 -0800

    ...

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,46 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+import { SpeechRecognition } from '@ionic-native/speech-recognition/ngx';
+import { TextToSpeech, TTSOptions } from "@ionic-native/text-to-speech/ngx";
+import { LocalNotifications } from '@ionic-native/local-notifications/ngx';
+
+/**/
+export class TextToSpeechMock extends TextToSpeech {
+  private synth:SpeechSynthesis = window.speechSynthesis;
+
+  speak(textOrOptions: string | TTSOptions): Promise<any>{
+    let text:string;
+    let rate:number;
+    let locale:string;
+    if (textOrOptions instanceof String){
+      text = textOrOptions;
+    }else{
+      text = textOrOptions.text;
+      rate = textOrOptions.rate;
+      locale = textOrOptions.locale;
+    }
+
+    let utterThis:SpeechSynthesisUtterance = new SpeechSynthesisUtterance(text);
+    if (rate){
+      utterThis.rate = rate;
+    }
+    if (locale){
+      utterThis.lang = locale;
+    }
+
+    this.synth.speak(utterThis);
+
+    return Promise.resolve(true);
+  }
+
+  stop(): Promise<any>{
+    this.synth.cancel();
+    return Promise.resolve(true);
+  }
+}
+/**/
+
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
@@ -16,6 +56,10 @@ import { AppRoutingModule } from './app-routing.module';
   providers: [
     StatusBar,
     SplashScreen,
+    SpeechRecognition,
+    //TextToSpeech,
+    { provide: TextToSpeech, useClass: TextToSpeechMock },
+    LocalNotifications,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
   ],
   bootstrap: [AppComponent]
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,10 +1,83 @@
 import { Component } from '@angular/core';
 
+import { SpeechRecognition } from '@ionic-native/speech-recognition/ngx';
+import { TextToSpeech } from '@ionic-native/text-to-speech/ngx';
+import { LocalNotifications } from '@ionic-native/local-notifications/ngx';
+
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+
+  /**/
+  speechText: string;
+  language: string;
+  talkSpeed: number;
+  /**/
+
+  constructor(
+    private speechRecognition: SpeechRecognition,
+    private tts: TextToSpeech,
+    private localNotifications: LocalNotifications,
+    //public navCtrl: NavController,
+  ) {
+      /**/
+      this.speechText = '';
+      this.language = 'en-US';
+      this.talkSpeed = 1;
+      /**/
+  }
+
+
+
+  ionViewDidLoad(){
+    // Check feature available
+    this.speechRecognition.isRecognitionAvailable()
+      .then((available: boolean) => console.log(available));
+  }
+
+
+
+  /**/
+  speakText(){
+    let talkRate = 1 + this.talkSpeed/10;
+    /**/
+    this.tts.speak({
+      text: this.speechText,
+      locale: this.language,
+      rate: talkRate
+    })
+    .then(() => console.log('Success'))
+    .catch((reason: any) => console.log(reason));
+    /**/
+    /**/
+    this.localNotifications.schedule({
+       text: 'Delayed ILocalNotification',
+       trigger: {at: new Date(new Date().getTime() + 3600)},
+       led: 'FF0000',
+       sound: null
+    });
+    /**/
+  }
+  /**/
+
+
+
+  listen(){
+    // Check permission
+    this.speechRecognition.hasPermission()
+      .then((hasPermission: boolean) => {
+        console.log(hasPermission)
+
+        this.speechRecognition.startListening(options)
+        .subscribe(
+          (matches: string[]) => console.log(matches),
+          (onerror) => console.log('error:', onerror)
+        );
+      });
+
+  }
 
 }
